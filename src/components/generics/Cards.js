684b4cf57134feef3a143ea5eb75ebe18d759774
Cabral <jcabral304@gmail.com>
2022-05-10 15:11:34 -0300

    Cards styles

--- src/components/generics/Cards.js
+++ src/components/generics/Cards.js
@@ -12,45 +12,58 @@ function Cards() {
   const { pathname } = location;
 
   return (
-    <div>
-      {data
-        && data.map((recipe, index) => {
-          let myRecipe = {};
-          if (pathname === '/drinks') {
-            myRecipe = {
-              id: recipe.idDrink,
-              thumb: recipe.strDrinkThumb,
-              recipeName: recipe.strDrink,
-            };
-          }
-          if (pathname === '/foods') {
-            myRecipe = {
-              id: recipe.idMeal,
-              thumb: recipe.strMealThumb,
-              recipeName: recipe.strMeal,
-            };
-          }
+    <div className="bg-brand-background">
+      <div className="max-w-2xl mx-auto py-16 px-4 sm:py-9 sm:px-6 lg:max-w-7xl lg:px-8">
+        <h2 className="text-2xl font-extrabold tracking-tight text-white">Recipes</h2>
+        <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+          {data
+            && data.map((recipe, index) => {
+              let myRecipe = {};
+              if (pathname === '/drinks') {
+                myRecipe = {
+                  id: recipe.idDrink,
+                  thumb: recipe.strDrinkThumb,
+                  recipeName: recipe.strDrink,
+                };
+              }
+              if (pathname === '/foods') {
+                myRecipe = {
+                  id: recipe.idMeal,
+                  thumb: recipe.strMealThumb,
+                  recipeName: recipe.strMeal,
+                };
+              }
 
-          return (
-            <Link
-              to={ `${pathname}/${myRecipe.id}` }
-              key={ index }
-              data-testid={ `${index}-recipe-card` }
-            >
-              <div>
-                <h2
-                  data-testid={ `${index}-card-name` }
+              return (
+                <Link
+                  to={`${pathname}/${myRecipe.id}`}
+                  key={index}
+                  data-testid={`${index}-recipe-card`}
+                  className="group relative"
                 >
-                  {myRecipe.recipeName}
-                </h2>
-                <img
-                  data-testid={ `${index}-card-img` }
-                  src={ myRecipe.thumb }
-                  alt=""
-                />
-              </div>
-            </Link>);
-        })}
+                  <div className="w-full min-h-80 bg-brand-background aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
+                    <img
+                      data-testid={`${index}-card-img`}
+                      src={myRecipe.thumb}
+                      alt={myRecipe.recipeName}
+                      className="w-full h-full object-center object-cover lg:w-full lg:h-full"
+                    />
+                    <div className="mt-4 flex justify-between">
+                      <div>
+                        <h2
+                          data-testid={`${index}-card-name`}
+                          aria-hidden="true"
+                          className="text-sm text-white"
+                        >
+                          {myRecipe.recipeName}
+                        </h2>
+                      </div>
+                    </div>
+                  </div>
+                </Link>);
+            })}
+        </div>
+      </div>
     </div>
   );
 }
